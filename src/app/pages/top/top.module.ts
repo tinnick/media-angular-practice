56d199ea676b09cd7fccdaef95a312db15369166
tinnick <tinnick@gmail.com>
2022-04-18 17:50:01 +0900

    2 files modified

--- src/app/pages/top/top.module.ts
+++ src/app/pages/top/top.module.ts
@@ -1,15 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
 
 import { TopRoutingModule } from './top-routing.module';
 import { TopPageComponent } from './components';
 
-
 @NgModule({
   declarations: [TopPageComponent],
-  imports: [
-    CommonModule,
-    TopRoutingModule
-  ]
+  imports: [CommonModule, MatButtonModule, TopRoutingModule],
 })
-export class TopModule { }
+export class TopModule {}
